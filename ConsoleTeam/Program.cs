400959104fdadf7db6cc780b40cad8f47cd2b048
bibekanandagithub <31163713+bibekanandagithub@users.noreply.github.com>
2017-08-20 22:50:25 +0530

    Add project files.

---
+++ ConsoleTeam/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Common;
+using Microsoft.TeamFoundation.Framework.Client;
+namespace ConsoleTeam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri tfsUri = (args.Length < 1) ?
+               new Uri("http://desktop-anh3ro7:8080/tfs") : new Uri(args[0]);
+
+            TfsConfigurationServer configurationServer =
+                TfsConfigurationServerFactory.GetConfigurationServer(tfsUri);
+
+            // Get the catalog of team project collections
+            ReadOnlyCollection<CatalogNode> collectionNodes = configurationServer.CatalogNode.QueryChildren(
+                new[] { CatalogResourceTypes.ProjectCollection },
+                false, CatalogQueryOptions.None);
+
+            // List the team project collections
+            foreach (CatalogNode collectionNode in collectionNodes)
+            {
+                // Use the InstanceId property to get the team project collection
+                Guid collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
+                TfsTeamProjectCollection teamProjectCollection = configurationServer.GetTeamProjectCollection(collectionId);
+
+                // Print the name of the team project collection
+                Console.WriteLine("Collection: " + teamProjectCollection.Name);
+
+                // Get a catalog of team projects for the collection
+                ReadOnlyCollection<CatalogNode> projectNodes = collectionNode.QueryChildren(
+                    new[] { CatalogResourceTypes.TeamProject },
+                    false, CatalogQueryOptions.None);
+
+                // List the team projects in the collection
+                foreach (CatalogNode projectNode in projectNodes)
+                {
+                    Console.WriteLine(" Team Project: " + projectNode.Resource.DisplayName);
+                }
+
+                Console.Read();
+            }
+
+        }
+    }
+}
