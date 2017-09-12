60c4d2471ceb652e89abbfdfc25ed00a1f0081c1
bibekanandagithub <bibekananda.panigrahi@outlook.com>
2017-09-12 10:38:52 +0530

    Added method

--- ConsoleTeam/TFS1.cs
+++ ConsoleTeam/TFS1.cs
@@ -10,52 +10,68 @@ using Microsoft.TeamFoundation.Framework.Client;
 using Microsoft.TeamFoundation.WorkItemTracking.Client;
 using Microsoft.TeamFoundation.TestManagement.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
+using System.IO;
 
 namespace ConsoleTeam
 {
     class TFS1
     {
         Uri CollectionUri = new Uri("http://desktop-anh3ro7:8080/tfs/");
-       
+
         public void GetAllWorkItem()
         {
             //Connect to the Team Foundation Server
             TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
             WorkItemStore wis = tpc.GetService<WorkItemStore>();
             //query will execute and store into the work items
-            Query wiq = new Query(wis,"Select * from WorkItems");
+            Query wiq = new Query(wis, "Select * from WorkItems");
             //All the collection will store here
             WorkItemCollection wic = wiq.RunQuery();
-            foreach(WorkItem wi in wic)
+            foreach (WorkItem wi in wic)
             {
                 Console.WriteLine(wi.Title.ToString() + "---" + wi.Fields["Work Item Type"].Value.ToString());
             }
 
             Console.Read();
-            
+
         }
         public void GetActiveWorkItem()
         {
             TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
             WorkItemStore wis = tpc.GetService<WorkItemStore>();
-            Query q = new Query(wis,"select * from WorkItem");
+            Query q = new Query(wis, "select * from WorkItem");
             WorkItemCollection wic = q.RunQuery();
-            foreach(WorkItem wi in wic)
+            foreach (WorkItem wi in wic)
             {
-               if(wi.State=="Active")
+                if (wi.State == "Active")
                 {
                     Console.WriteLine(wi.Title.ToString() + "---" + wi.Fields["Work Item Type"].Value.ToString());
                 }
             }
             Console.Read();
         }
+
+        public void GetListofRequirement()
+        {
+            TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
+            WorkItemStore wis = tpc.GetService<WorkItemStore>();
+            Query Q = new Query(wis, "Select * from WorkItem");
+            WorkItemCollection wic = Q.RunQuery();
+            foreach (WorkItem w in wic)
+            {
+                if (w.Fields["Work Item Type"].Value.ToString() == "Requirement")
+                {
+                    Console.WriteLine(w.Title.ToString());
+                }
+            }
+        }
         //single work item by query
         public void GetSpecificworkitem(int WorkItemID)
         {
             TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
             WorkItemStore wis = tpc.GetService<WorkItemStore>();
-            WorkItemCollection wic = new Query(wis, "select * from WorkItem where ID='"+WorkItemID+"'").RunQuery();
-            foreach(WorkItem wi in wic)
+            WorkItemCollection wic = new Query(wis, "select * from WorkItem where ID='" + WorkItemID + "'").RunQuery();
+            foreach (WorkItem wi in wic)
             {
                 Console.WriteLine(wi.Title.ToString() + "---" + wi.Fields["Work Item Type"].Value.ToString());
             }
@@ -67,13 +83,13 @@ namespace ConsoleTeam
             WorkItemStore wis = tpc.GetService<WorkItemStore>();
             WorkItemCollection wic = new Query(wis, "select * from WorkItem").RunQuery();
             WorkItem wi = wis.GetWorkItem(WorkItemID);
-            Console.WriteLine(wi.Title + " " + wi.Type.Name.ToString()+"  "+"Area Path="+wi.Fields["Area Path"].Value.ToString());
+            Console.WriteLine(wi.Title + " " + wi.Type.Name.ToString() + "  " + "Area Path=" + wi.Fields["Area Path"].Value.ToString());
 
         }
         public void GetAllTestPlan()
         {
             TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
-            ITestManagementTeamProject itp = tpc.GetService<ITestManagementService>().GetTeamProject("Techbrother_Team"); 
+            ITestManagementTeamProject itp = tpc.GetService<ITestManagementService>().GetTeamProject("Techbrother_Team");
 
         }
         public void GetTestPoints(int suidid)
@@ -81,13 +97,13 @@ namespace ConsoleTeam
             TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
             ITestManagementTeamProject itp = tpc.GetService<ITestManagementService>().GetTeamProject("Techbrother_Team");
             ITestSuiteBase testsuite = itp.TestSuites.Query("select * from TestSuite where id=" + suidid + "").FirstOrDefault();
-            ITestPointCollection ipc= testsuite.Plan.QueryTestPoints("select * from TestPoint where Suiteid="+suidid+""); 
+            ITestPointCollection ipc = testsuite.Plan.QueryTestPoints("select * from TestPoint where Suiteid=" + suidid + "");
 
-            foreach(ITestPoint tp in ipc)
+            foreach (ITestPoint tp in ipc)
             {
-                Console.WriteLine(tp.SuiteId + "----" + tp.TestCaseId+"-----"+tp.State.ToString()+"---"+tp.TestCaseWorkItem.Title);
+                Console.WriteLine(tp.SuiteId + "----" + tp.TestCaseId + "-----" + tp.State.ToString() + "---" + tp.TestCaseWorkItem.Title);
             }
-            
+
         }
         public void GetSharedQueries()
         {
@@ -102,22 +118,22 @@ namespace ConsoleTeam
             queryFolder = queryItem as QueryFolder;
             foreach (var item in queryFolder)
             {
-             
+
                 listQueries.Add(item.Name);
-              //  Console.WriteLine(item.Name);
+                //  Console.WriteLine(item.Name);
             }
             var variables = new Dictionary<string, string>() { { "project", "Techbrother_Team" } };
 
             QueryHierarchy queryRoot = wis.Projects[0].QueryHierarchy;
             QueryFolder folder = (QueryFolder)queryRoot["Shared Queries"];
             QueryDefinition query = (QueryDefinition)folder["BugDisplayQueries"];
-            queryResults = wis.Query(query.QueryText,variables);
-            foreach(FieldDefinition field in queryResults.DisplayFields)
+            queryResults = wis.Query(query.QueryText, variables);
+            foreach (FieldDefinition field in queryResults.DisplayFields)
             {
                 Console.WriteLine(field.Name);
             }
             Console.WriteLine("-----------------");
-            foreach(WorkItem w in queryResults)
+            foreach (WorkItem w in queryResults)
             {
                 Console.WriteLine(w.Title);
             }
@@ -137,7 +153,7 @@ namespace ConsoleTeam
 
             // Get Changesets
             var changesets = vcs.QueryHistory("$/Techbrother_Team/Main",
-             
+
               VersionSpec.Latest,
               0,
               RecursionType.Full,
@@ -151,8 +167,74 @@ namespace ConsoleTeam
 
             foreach (var changeset in changesets)
             {
-                Console.WriteLine(changeset.ChangesetId+"--"+changeset.OwnerDisplayName+changeset.CreationDate);
+                Console.WriteLine(changeset.ChangesetId + "--" + changeset.OwnerDisplayName + changeset.CreationDate);
             }
+        }
+
+        public void FindContentofFileinTFS()
+        {
+            string teamProjectCollectionUrl = "http://desktop-anh3ro7:8080/tfs/";
+            string filePath = @"$/Techbrother_Team/Main/ConsolewithtfsFeature/ConsolewithtfsFeature/Program.cs";
+
+            // Get the version control server
+            TfsTeamProjectCollection teamProjectCollection = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(teamProjectCollectionUrl));
+            VersionControlServer versionControlServer = teamProjectCollection.GetService<VersionControlServer>();
+
+            // Get the latest Item for filePath
+            Item item = versionControlServer.GetItem(filePath, VersionSpec.Latest);
+
+            // Download and display content to console
+            string fileString = string.Empty;
+
+            using (Stream stream = item.DownloadFile())
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+
+                    // Use StreamReader to read MemoryStream created from byte array
+                    using (StreamReader streamReader = new StreamReader(new MemoryStream(memoryStream.ToArray())))
+                    {
+                        fileString = streamReader.ReadToEnd();
+                    }
+                }
+            }
+
+            Console.WriteLine(fileString);
+            Console.ReadLine();
+        }
+        public void GetTestSuiteByName()
+        {
+            TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
+            ITestManagementTeamProject itp = tpc.GetService<ITestManagementService>().GetTeamProject("Techbrother_Team");
+            string query = string.Concat("SELECT * FROM TestSuite");
+            var firstMatchingSuite = itp.TestSuites.Query(query);
+            foreach (IStaticTestSuite childSuite in firstMatchingSuite)
+            {
+                Console.WriteLine("{0} Suite", childSuite.Title);
+
+            }
+            ITestPlan testPlan = itp.TestPlans.Find(3);
+            Console.WriteLine("Root Suite: {0}", testPlan.RootSuite.Title);
+
+        }
+        public void GetAllTestPlanALL()
+        {
+            TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(CollectionUri);
+            ITestManagementTeamProject itp = tpc.GetService<ITestManagementService>().GetTeamProject("Techbrother_Team");
+
+            ITestPlanCollection plans = itp.TestPlans.Query("Select * From TestPlan");
+            foreach (ITestPlan plan in plans)
+            {
+                //Console.WriteLine(plan.RootSuite.Title);
+                //  Console.WriteLine(plan.RootSuite.TestCases);
+                foreach (ITestSuiteEntry ie in plan.RootSuite.TestCases)
+                {
+                    Console.WriteLine(ie.Title);
+                }
+                Console.WriteLine("-------------------------------");
+            }
+
         }
     }
 }
